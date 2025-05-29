54e92811371332a66bca88000250ee1b46d32a92
kakegithub <ricardoespineiradiaz@gmail.com>
2025-05-29 19:46:18 +0200

    v1.0

--- 058_Practicas.py
+++ 058_Practicas.py
@@ -1,7 +1,7 @@
 # Destreza 1: Uso de métodos de cadena
 
-""" # Separe los valores numéricos de los valores de texto en una cadena utilizando .split().
- """
+"""# Separe los valores numéricos de los valores de texto en una cadena
+utilizando .split()."""
 # Iterar sobre los elementos de una cadena.
 
 # Compruebe si el elemento contiene letras con .isalpha().
@@ -24,16 +24,17 @@ def sales_price(item_and_price):
     # For each element "x" in the split variable "item_or_price"
     for x in item_or_price:
         # Check if the element is a letter
-            if x.isalpha():
-            # If true, assign the element to the "item" string variable and add a space
-            # for any item names containing multiple words, like "Winter fleece jacket".
+        if x.isalpha():
+            # If true, assign the element to the "item" string variable and add a
+            # space for any item names containing multiple words, like
+            # "Winter fleece jacket".
             item += x + " "
-            # Else, if x is a number (if x.isalpha() is false):
+        # Else, if x is a number (if x.isalpha() is false):
         else:
             # Assign the element to the "price" string variable.
             price = x
-    # Strip the extra space to the right of the last "item" word
-    item = item.strip()
+        # Strip the extra space to the right of the last "item" word
+        item = item.strip()
 
     # Return the item name and price formatted in a sentence
     return "{} are on sale for ${}".format(item, price)
